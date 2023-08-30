2ac007946bed6602d2896f4ef79fc8483d05a94c
johntelforduk <jtelford1@gmail.com>
2023-08-30 17:59:17 +0100

    Refactor and update README.

--- README.md
+++ README.md
@@ -1,10 +1,27 @@
 # Google Calendar Bulk Delete
-Utility to bulk delete events from Google Calendar.
+Utility to bulk delete duplicate events from Google Calendar. I made it when an iOS phone app accidentally created 1000s of copies of the same event in my Google Calendar.
+
+Before...
+![Before](./media/before.jpg)
+
+After...
+![After](./media/after.jpg)
 
 It is based on this package, [gcsa](https://google-calendar-simple-api.readthedocs.io/en/latest/index.html).
+
+You should follow the [Getting Started](https://google-calendar-simple-api.readthedocs.io/en/latest/getting_started.html) guide for advice on providing the program with access to the Google Calendar API for your calendar(s).
 
 #### Installation
 ```commandline
 pip install gcsa
 ```
+The following global variables should be adjusted in `bulk_delete.py` to configure the program, 
+```
+CALENDAR_ID = 'youremail AT gmail.com'
+START = (1/Jan/2023)[12:00]
+TEST = False
+```
 Then run the program using `python bulk_delete.py`.
+
+#### Caution
+This program is potentially destructive, as it can delete events from your calendars. It is recommended to backup your calendar data before using it.
--- bulk_delete.py
+++ bulk_delete.py
@@ -1,35 +1,28 @@
 from beautiful_date import Jan
 from gcsa.google_calendar import GoogleCalendar
 
-calendar = GoogleCalendar('myemail@gmail.com')
+CALENDAR_ID = 'youremail AT gmail.com'
+START = (1/Jan/2023)[12:00]
+TEST = False
 
-start = (22/Jan/2023)[12:00]
+calendar = GoogleCalendar(CALENDAR_ID)
 
-# Count the number of copies of each event (1 means no copy).
-event_dict = {}                 # key=event as string, value=number of copies.
-for event in calendar.get_events(time_min=start):
+found_events = []
+event_count = 0
+
+for event in calendar.get_events(time_min=START):
     event_str = str(event)
 
-    if event_str in event_dict:
-        event_dict[event_str] = event_dict[event_str] + 1
-    else:
-        event_dict[event_str] = 1
-
-# Make a dictionary of duplicate events.
-duplicate_dict = {}
-for key in event_dict:
-    value = event_dict[key]
-
-    # The 'None - None' event seems to be some kind of dummy, and doesn't need to be deleted.
-    if value > 1 and key != 'None - None':
-        duplicate_dict[key] = value
-
-print(duplicate_dict)
-
-# Delete duplicate events.
-event_count = 0
-for event in calendar.get_events(time_min=start):
-    if str(event) in duplicate_dict:
+    if event_str in found_events:
         event_count += 1
-        print(event_count, event)
-        GoogleCalendar.delete_event(calendar, event)
+        if TEST:
+            message = "Test mode. Would have deleted: "
+        else:
+            message = "Live mode. Event deleted: "
+            GoogleCalendar.delete_event(calendar, event)
+
+        message += event_str + ". Total events: " + str(event_count)
+        print(message)
+
+    else:
+        found_events.append(event_str)
